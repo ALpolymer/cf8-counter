644ab3e8f960ba4a2371ff39f59fcf022af77ac3
Alexandros Polymeros <alpolymer@gmail.com>
2025-11-15 13:30:16 +0200

    feat: add useCounter hook

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,51 +1,14 @@
-import * as React from "react";
 import CounterButton from "./ui/CounterButton.tsx";
+import useCounter from "../hooks/useCounter.tsx";
 
-type CounterState = {
-    count: number,
-    lastAction?: string,
-    time?: string
-}
+
 
 const Counter = () => {
 
-    const getCurrentTime = () => new Date().toLocaleTimeString();
-
-    const [counterState, setCounterState] = React.useState<CounterState>({
-        count: 0,
-        lastAction: "",
-        time: "",
-    })
-
-    const increaseCount = () => {
-        setCounterState({
-            count: counterState.count + 1,
-            lastAction: "Increase",
-            time: getCurrentTime(),
-        })
-    }
-
-    const decreaseCount = () => {
-        if (counterState.count > 0) {
-            setCounterState({
-                count: counterState.count - 1,
-                lastAction: "Decrease",
-                time: getCurrentTime(),
-            })
-        }
-    }
-
-    const resetCount = () => {
-        setCounterState({
-            count: 0,
-            lastAction: "Reset",
-            time: getCurrentTime(),
-        })
-        }
-
+  const { count, increaseCount, resetCount, decreaseCount, time, lastAction } = useCounter();
     return (
         <div className="flex flex-col justify-center items-center gap-4">
-           <p className="text-5xl font-bold">{counterState.count}</p>
+           <p className="text-5xl font-bold">{count}</p>
 
             <div className="flex justify-center items-center gap-4">
                 <CounterButton
@@ -55,16 +18,16 @@ const Counter = () => {
                 <CounterButton
                     name={"Decrease"}
                     onClick={decreaseCount}
-                    disabled={counterState.count === 0}
+                    disabled={count === 0}
                 />
                 <CounterButton
                     name={"Reset"}
                     onClick={resetCount}
-                    disabled={counterState.count === 0}
+                    disabled={count === 0}
                 />
             </div>
 
-            <p>Last Change:{` ${!counterState.lastAction ? "" : `${counterState.lastAction} at ${counterState.time}`} `}</p>
+            <p>Last Change:{` ${!lastAction ? "" : `${lastAction} at ${time}`} `}</p>
         </div>
 
     );
---
+++ src/hooks/useCounter.tsx
@@ -0,0 +1,57 @@
+import {useState} from "react";
+import * as React from "react";
+
+type CounterState = {
+    count: number,
+    lastAction?: string,
+    time?: string
+}
+
+
+
+const UseCounter = () => {
+    const getCurrentTime = () => new Date().toLocaleTimeString();
+
+    const [counterState, setCounterState] = React.useState<CounterState>({
+        count: 0,
+        lastAction: "",
+        time: "",
+    })
+
+    const increaseCount = () => {
+        setCounterState({
+            count: counterState.count + 1,
+            lastAction: "Increase",
+            time: getCurrentTime(),
+        })
+    }
+
+    const decreaseCount = () => {
+        if (counterState.count > 0) {
+            setCounterState({
+                count: counterState.count - 1,
+                lastAction: "Decrease",
+                time: getCurrentTime(),
+            })
+        }
+    }
+
+    const resetCount = () => {
+        setCounterState({
+            count: 0,
+            lastAction: "Reset",
+            time: getCurrentTime(),
+        })
+    }
+
+    return {
+        count: counterState.count,
+        lastAction: counterState.lastAction,
+        time: counterState.time,
+        increaseCount,
+        decreaseCount,
+        resetCount,
+    }
+};
+
+export default UseCounter;
